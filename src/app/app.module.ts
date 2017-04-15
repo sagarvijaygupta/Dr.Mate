038521ee39488d14e2f088925f9eec4f0e92bafe
Hitesh Ramchandani <hitr9831@gmail.com>
2017-04-15 17:57:18 +0530

    Add Material Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  ], 
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
