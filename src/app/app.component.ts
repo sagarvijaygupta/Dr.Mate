b34d47a9aeafc9c3d8ed32ed6a3ac4c00d893d9f
sanidhya280 <sanidhya280@gmail.com>
2017-04-18 16:24:43 +0530

    Integrate app module with other components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DISEASES } from './mock-disease'; 
 import 'hammerjs';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { CheckupComponent } from './checkup.component';
+import { HomeComponent } from './home.component';
+import { DiseaseService } from './disease.service';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckupComponent,
+    HomeComponent
   ], 
   imports: [
     BrowserModule,
@@ -16,8 +23,25 @@ import { AppComponent } from './app.component';
     HttpModule,
     BrowserAnimationsModule,
     MaterialModule.forRoot(),
+    RouterModule.forRoot([
+        {
+          path: 'checkup',
+          component: CheckupComponent
+        },
+        {
+          path: 'home',
+          component: HomeComponent
+        },
+        {
+          path: '',
+          redirectTo: '/home',
+          pathMatch: 'full'
+        }
+       ]
+    ),
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DiseaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
